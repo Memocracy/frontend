419d2ba1dada6873ad40151dbac69bc32d568b40
Konrad Fedorczyk <contact@realhe.ro>
2021-12-24 12:08:13 +0100

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,17 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          quality: 70,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
