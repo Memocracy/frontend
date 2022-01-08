a1c9cf3042561f29ff35bed7f4b0a9dd9fa1062c
Konrad Fedorczyk <contact@realhe.ro>
2022-01-08 19:59:01 +0100

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     quotes: ["warn", "double", { allowTemplateLiterals: true }],
     "quote-props": ["error", "as-needed"],
+    "import/prefer-default-export": "off"
   },
   extends: ["gatsby-standard", "airbnb-base", "react-app", "prettier"],
 };
