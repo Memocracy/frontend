19ddf3fefaed5cc6402be1ee546242feb18ebd74
Konrad Fedorczyk <contact@realhe.ro>
2022-01-09 19:06:03 +0100

    Add full support for WP content

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-console */
+const fs = require("fs");
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     {
-      allWpPost {
+      allWpContentNode {
         nodes {
           id
           uri
           slug
+          __typename
         }
       }
     }
@@ -15,21 +19,35 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.error("There was an error fetching posts", result.errors);
   }
 
-  const { allWpPost } = result.data;
+  const { allWpContentNode } = result.data;
 
   // Define the template to use
-  const template = require.resolve(`./src/components/Pages/Post`);
-
-  if (allWpPost.nodes.length) {
+  if (allWpContentNode.nodes.length) {
     // eslint-disable-next-line array-callback-return
-    allWpPost.nodes.map((post) => {
-      actions.createPage({
-        // It's best practice to use the uri field from WPGraphQL nodes when
-        // building
-        path: post.slug,
-        component: template,
-        context: post,
-      });
+    allWpContentNode.nodes.map((node) => {
+      let template;
+
+      // eslint-disable-next-line no-underscore-dangle
+      const typeName = node.__typename.substr(2);
+      const templatePath = `./src/components/Pages/${typeName}/${typeName}.jsx`;
+
+      try {
+        if (fs.existsSync(templatePath)) {
+          template = require.resolve(templatePath)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+
+      if (template && node.slug) {
+        actions.createPage({
+          // It's best practice to use the uri field from WPGraphQL nodes when
+          // building
+          path: node.slug,
+          component: template,
+          context: node,
+        });
+      }
     });
   }
 };
--- src/components/Pages/Post/Post.jsx
+++ src/components/Pages/Post/Post.jsx
@@ -1,3 +1,29 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { Meta } from "../../Other/Meta";
 
-export const Post = ({ children }) => <div>aaa{children}</div>;
+const Post = ({
+  data: {
+    wpPost: { title, content, id },
+  },
+}) => {
+  return (
+    <div>
+      <Meta title={title} />
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    wpPost(id: { eq: $id }) {
+      id
+      title
+      content
+    }
+  }
+`;
+
+export default Post;
