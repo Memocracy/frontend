fb8d01b9751097c45b4fc7b85bc0db4f16bf0650
Konrad Fedorczyk <contact@realhe.ro>
2022-01-24 10:56:39 +0100

    Add vertical spacer

--- src/lib/cssHelpers.js
+++ src/lib/cssHelpers.js
@@ -2,6 +2,7 @@ import { capitalize } from "./stringHelpers";
 import * as fontSizes from "../assets/styles/FontSizes.module.scss";
 import * as lineHeights from "../assets/styles/LineHeights.module.scss";
 import * as horizontalSpacing from "../assets/styles/HorizontalSpacing.module.scss";
+import * as verticalSpacing from "../assets/styles/VerticalSpacing.module.scss";
 import * as standard from "../assets/styles/Standard.module.scss";
 
 /**
@@ -31,6 +32,36 @@ export const typographer = (
   if (lineHeights[lineHeightName]) {
     classes.push(lineHeights[lineHeightName]);
   }
+
+  // Spacing bottom
+  if (textAlign) {
+    const textAlignName = `textAlign${capitalize(textAlign)}`;
+
+    if (standard[textAlignName]) {
+      classes.push(standard[textAlignName]);
+    }
+  }
+
+  classes.push(horizontalSpacer(
+    paddingTop ,
+    paddingBottom,
+    marginTop,
+    marginBottom,
+  ));
+
+  return classes.join(" ");
+};
+
+/**
+ * @returns {string} classes A list of ready classes
+ */
+export const horizontalSpacer = (
+  paddingTop ,
+  paddingBottom,
+  marginTop,
+  marginBottom
+) => {
+  const classes = [];
 
   // Spacing top
   if (paddingTop) {
@@ -68,14 +99,35 @@ export const typographer = (
     }
   }
 
-  // Spacing bottom
-  if (textAlign) {
-    const textAlignName = `textAlign${capitalize(textAlign)}`;
+  return classes.join(" ");
+}
 
-    if (standard[textAlignName]) {
-      classes.push(standard[textAlignName]);
+/**
+ * @returns {string} classes A list of ready classes
+ */
+ export const verticalSpacer = (
+  paddingLeft,
+  paddingRight
+) => {
+  const classes = [];
+
+  // Padding left
+  if (paddingLeft) {
+    const paddingLeftName = `paddingLeft${capitalize(paddingLeft)}`;
+
+    if (verticalSpacing[paddingLeftName]) {
+      classes.push(verticalSpacing[paddingLeftName]);
+    }
+  }
+
+  // Padding right
+  if (paddingRight) {
+    const paddingRightName = `paddingRight${capitalize(paddingRight)}`;
+
+    if (verticalSpacing[paddingRightName]) {
+      classes.push(verticalSpacing[paddingRightName]);
     }
   }
 
   return classes.join(" ");
-};
+}
