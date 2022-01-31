e0c0616efe51aca678d52bfc6957b545ebc74007
Konrad Fedorczyk <contact@realhe.ro>
2022-01-31 10:28:48 +0100

    Update template for generic pages

--- src/components/Pages/Page/Page.jsx
+++ src/components/Pages/Page/Page.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { Media } from "gatsby-plugin-fresnel";
+import { Meta } from "../../Other/Meta";
+import { Container, ContainerLimited } from "../../Molecules/Container";
+import { ContentHeader, Content } from "../../Molecules/Content";
+import { useFooterMenu } from "../../../hooks";
+import { HeaderMobile, Header } from "../../Organisms/Header";
+import { FooterMobile, Footer } from "../../Organisms/Footer";
 
-const Post = ({ data }) => {
+
+
+const Page = ({ data }) => {
+  const footerMenuItems = useFooterMenu();
+  const { title, content } = data.wpPage;
+  const contents = <Content content={content} />;
+
   return (
-    <div>
-      <pre>{JSON.stringify(data, null, 4)}</pre>
-    </div>
+    <>
+      <Meta title={title} />
+      <Media lessThan="md">
+        {/* Mobile version */}
+        <HeaderMobile />
+        <Container>
+          <ContentHeader title={title} size="h3" paddingBottom="md" />
+          {contents}
+        </Container>
+        <FooterMobile items={footerMenuItems} />
+      </Media>
+      <Media greaterThanOrEqual="md">
+        {/* Destkop version */}
+        <Header />
+        <Container>
+          <ContainerLimited>
+            <ContentHeader title={title} size="h1" paddingBottom="md" />
+            {contents}
+          </ContainerLimited>
+        </Container>
+        <Footer items={footerMenuItems} />
+      </Media>
+    </>
   );
 };
 
@@ -19,4 +52,4 @@ export const query = graphql`
   }
 `;
 
-export default Post;
+export default Page;
