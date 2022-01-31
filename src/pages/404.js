ddcb5f70cd240697819ce1a2af705c09eff19ef4
Konrad Fedorczyk <contact@realhe.ro>
2022-01-31 09:41:40 +0100

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,42 @@
-import * as React from "react";
+import React from "react";
+import { Media } from "gatsby-plugin-fresnel";
+import { Meta } from "../components/Other/Meta";
+import { Container } from "../components/Molecules/Container";
+import { ContentHeader, Content } from "../components/Molecules/Content";
+import { useFooterMenu } from "../hooks";
+import { HeaderMobile, Header } from "../components/Organisms/Header";
+import { FooterMobile, Footer } from "../components/Organisms/Footer";
 
-// markup
-const NotFoundPage = () => (
-  <main>
-    <title>Not found</title>
-    <h1>Page not found</h1>
-  </main>
-);
+const title = "not found";
+const headingTitle = "Not found";
+const message404 = "<p>Sorry, there's nothing here</p>";
 
-export default NotFoundPage;
+const NotFound = () => {
+  const footerMenuItems = useFooterMenu();
+
+  return (
+    <>
+      <Meta title={title} />
+      <Media lessThan="md">
+        {/* Mobile version */}
+        <HeaderMobile />
+        <Container>
+          <ContentHeader title={headingTitle} size="h3" paddingBottom="md" />
+          <Content content={message404} />
+        </Container>
+        <FooterMobile items={footerMenuItems} />
+      </Media>
+      <Media greaterThanOrEqual="md">
+        {/* Destkop version */}
+        <Header />
+        <Container>
+          <ContentHeader title={headingTitle} size="h1" paddingBottom="md" />
+          <Content content={message404} />
+        </Container>
+        <Footer items={footerMenuItems} />
+      </Media>
+    </>
+  );
+};
+
+export default NotFound;
