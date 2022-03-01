604d3510dd5ab0aec9eb558acdfe36b5ce3103e7
Konrad Fedorczyk <contact@realhe.ro>
2022-03-01 13:45:58 +0100

    Add a new string helper

--- src/lib/stringHelpers.js
+++ src/lib/stringHelpers.js
@@ -3,3 +3,19 @@ export const isEmailValid = (email) => {
 };
 
 export const capitalize = s => (s && s[0].toUpperCase() + s.slice(1)) || "";
+
+/**
+ * Check type of a link.
+ * @param {string} link
+ * @returns
+ */
+export const getTypeOfLink = (link) => {
+  if (link.includes("mailto:")) {
+    return "mail";
+  // eslint-disable-next-line no-else-return
+  } else if(link.match(/^https?:\/\//)) {
+    return "external";
+  } else {
+    return "internal";
+  }
+}
