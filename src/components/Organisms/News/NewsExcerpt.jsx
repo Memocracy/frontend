fec54a4e974820d35a71b459b4c08c1b40d5f0a8
Konrad Fedorczyk <contact@realhe.ro>
2022-03-08 21:38:21 +0100

    Add image support for newsexcerpt

--- src/components/Organisms/News/NewsExcerpt.jsx
+++ src/components/Organisms/News/NewsExcerpt.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import * as newsExcerptStyles from "./NewsExcerpt.module.scss";
 
 export const NewsExcerpt = ({ data, link, additionalClasses = [] }) => {
-  return <h1>Hello world</h1>;
-}
+  const featuredImage = {
+    data: data.fieldsForNews.customThumbnail?.localFile?.childImageSharp
+      ?.gatsbyImageData,
+    alt: data.fieldsForNews.customThumbnail?.alt || ``,
+  };
+
+  return (
+    <article>
+      <div>
+        {featuredImage?.data && (
+          <GatsbyImage image={featuredImage.data} alt={featuredImage.alt} />
+        )}
+      </div>
+    </article>
+  );
+};
