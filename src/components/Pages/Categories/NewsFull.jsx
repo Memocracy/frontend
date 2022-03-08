c294367b0586185497e19c618d08be207e11d0b7
Konrad Fedorczyk <contact@realhe.ro>
2022-03-08 19:49:36 +0100

    Add full news view

--- src/components/Pages/Categories/NewsFull.jsx
+++ src/components/Pages/Categories/NewsFull.jsx
@@ -6,7 +6,11 @@ import { Meta } from "../../Other/Meta";
 import { FooterMobile, Footer } from "../../Organisms/Footer";
 import { ContentHeader } from "../../Molecules/Content";
 import { Header, HeaderMobile } from "../../Organisms/Header";
-import { InnerContainer, Container, GridContainer } from "../../Molecules/Container";
+import {
+  InnerContainer,
+  Container,
+  GridContainer,
+} from "../../Molecules/Container";
 import { Pagination } from "../../Molecules/Pagination";
 import { News as NewsComponent } from "../../Organisms/News";
 import { ImagePreview } from "../../Molecules/ImagePreview";
@@ -19,13 +23,30 @@ const pageDescription = "Memocracy project news";
 const NewsFull = ({ data }) => {
   const footerMenuItems = useFooterMenu();
 
+  const NewsRendered = <NewsComponent key={uuid()} data={data.wpPost} />;
+
   return (
     <>
-      { JSON.stringify(data) }
+      <Meta title={title} description={pageDescription} />
+      <ImagePreview />
+      <Media lessThan="md">
+        {/* Mobile version */}
+        <HeaderMobile />
+
+        {NewsRendered}
+
+        <FooterMobile items={footerMenuItems} />
+      </Media>
+
+      <Media greaterThanOrEqual="md">
+        {/* Destkop version */}
+        <Header />
+        <Container>{NewsRendered}</Container>
+        <Footer items={footerMenuItems} />
+      </Media>
     </>
   );
 };
-
 
 export const query = graphql`
   query ($id: String) {
@@ -33,6 +54,17 @@ export const query = graphql`
       id
       title
       content
+      date(formatString: "YYYY-MM-DD")
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(quality: 85, placeholder: BLURRED, width: 1000)
+            }
+          }
+        }
+      }
     }
   }
 `;
