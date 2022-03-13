4b3dad1cbea06fcca06168f3057823ad2044e225
Konrad Fedorczyk <contact@realhe.ro>
2022-03-13 19:40:19 +0100

    Refactor menu component

--- src/components/Molecules/Menu/Menu.jsx
+++ src/components/Molecules/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
+import { InteligentLink } from "../../Atoms/InteligentLink";
 import { getTypeOfLink } from "../../../lib/stringHelpers";
-
 
 /**
  * Renders a <Menu /> component
@@ -14,22 +14,17 @@ export const Menu = ({ items = [] }) => {
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            {/* Internal type of a link */}
-            {getTypeOfLink(item.path) === "internal" && <Link
-              to={item.path}
-              activeClassName="active"
-              partiallyActive={item.path !== "/"}
-            >
-              {item.label}
-            </Link>}
-            {/* External type of a link */}
-            {getTypeOfLink(item.path) !== "internal" && <a
-              href={item.path}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {item.label}
-            </a>}
+            <InteligentLink path={item.path} label={item.label} />
+            {item.childItems?.nodes.length > 0 && (
+              <ul>
+                {item.childItems.nodes.map((subItem) => (
+                  <li key={subItem.id}>
+                    <InteligentLink path={subItem.path} label={subItem.label} />
+                  </li>
+                ))}
+
+              </ul>
+            )}
           </li>
         ))}
       </ul>
